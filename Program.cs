c6dba69e57486bffc44b2b38781833ffd7d57914
James Mead <jdmead@asu.edu>
2019-08-06 15:25:46 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //MULTIPLICATION TABLE
+            int[,] multTable = new int[10, 10];
+            for (int i = 1; i < 10; i++)
+                {
+                for (int j = 1; j < 10; j++)
+                {
+                    multTable[i,j] = i*j;
+                    Console.Write(multTable[i,j]+ " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
